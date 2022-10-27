552eadb798e057d5e93d70fa959fe416fb3600bb
BetterCallAle <dap22091998@gmail.com>
2022-10-27 15:11:26 +0200

    todo list is working with linethrough when isDone === tre

--- js/script.js
+++ js/script.js
@@ -3,7 +3,21 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            toDoList: [{
+                toDo: "Impazzire con Js",
+                isDone: false
+            },
             
+            {
+                toDo: "Guardare per la trentesima volta How I Met Your Mother",
+                isDone: true
+            },
+
+            {
+                toDo: "Comprare il caffè",
+                isDone: false
+            }]
+
         }
     }
 }).mount("#app")
